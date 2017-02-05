e71f68fa96879e5117bb4597ed52d326c3d25abb
Peter8586 <peterpineda@Peters-MacBook-Pro-2.local>
2017-02-05 11:08:52 -0800

    secong commit

---
+++ twotwo/twotwo/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  twotwo
+//
+//  Created by Peter Pineda on 2/5/17.
+//  Copyright © 2017 Peter Pineda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World")
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
